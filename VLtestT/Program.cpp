934879245bb511e6c14f192cc1689f24dd47af7a
dud0 <dud0@ghusto>
2011-10-17 21:48:42 +0200

    Gui zmenene na Qt4

--- VLtestT/Program.cpp
+++ VLtestT/Program.cpp
@@ -1,51 +1,16 @@
-/**************************************************************************************/
-/*                                                                                    */
-/*  Visualization Library                                                             */
-/*  http://www.visualizationlibrary.org                                               */
-/*                                                                                    */
-/*  Copyright (c) 2005-2010, Michele Bosi                                             */
-/*  All rights reserved.                                                              */
-/*                                                                                    */
-/*  Redistribution and use in source and binary forms, with or without modification,  */
-/*  are permitted provided that the following conditions are met:                     */
-/*                                                                                    */
-/*  - Redistributions of source code must retain the above copyright notice, this     */
-/*  list of conditions and the following disclaimer.                                  */
-/*                                                                                    */
-/*  - Redistributions in binary form must reproduce the above copyright notice, this  */
-/*  list of conditions and the following disclaimer in the documentation and/or       */
-/*  other materials provided with the distribution.                                   */
-/*                                                                                    */
-/*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND   */
-/*  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED     */
-/*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE            */
-/*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR  */
-/*  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    */
-/*  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;      */
-/*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON    */
-/*  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT           */
-/*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS     */
-/*  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                      */
-/*                                                                                    */
-/**************************************************************************************/
-
 #include <vlCore/VisualizationLibrary.hpp>
-#include <vlGLUT/GLUTWindow.hpp>
+#include <vlQt4/Qt4Widget.hpp>
 #include "TetraApplet.hpp"
 
 using namespace vl;
+using namespace vlQt4;
 
-int main ( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-  /* init GLUT */
-  int pargc = argc;
-  glutInit( &pargc, argv );
+  QApplication app(argc, argv);
 
   /* init Visualization Library */
   VisualizationLibrary::init();
-
-  /* install Visualization Library shutdown function */
-  atexit( vlGLUT::atexit_visualization_library_shutdown );
 
   /* setup the OpenGL context format */
   OpenGLContextFormat format;
@@ -60,12 +25,12 @@ int main ( int argc, char *argv[] )
   /* create the applet to be run */
   ref<Applet> applet = new TetraApplet;
   applet->initialize();
-  /* create a native GLUT window */
-  ref<vlGLUT::GLUTWindow> glut_window = new vlGLUT::GLUTWindow;
+  /* create a native Qt4 window */
+  ref<vlQt4::Qt4Widget> qt4_window = new vlQt4::Qt4Widget;
   /* bind the applet so it receives all the GUI events related to the OpenGLContext */
-  glut_window->addEventListener(applet.get());
+  qt4_window->addEventListener(applet.get());
   /* target the window so we can render on it */
-  applet->rendering()->as<Rendering>()->renderer()->setFramebuffer( glut_window->framebuffer() );
+  applet->rendering()->as<Rendering>()->renderer()->setFramebuffer( qt4_window->framebuffer() );
   /* black background */
   applet->rendering()->as<Rendering>()->camera()->viewport()->setClearColor( black );
   /* define the camera position and orientation */
@@ -75,19 +40,22 @@ int main ( int argc, char *argv[] )
   mat4 view_mat = mat4::getLookAt(eye, center, up);
   applet->rendering()->as<Rendering>()->camera()->setViewMatrix( view_mat );
   /* Initialize the OpenGL context and window properties */
-  int x = 0;
-  int y = 0;
+  int x = 10;
+  int y = 10;
   int width = 512;
   int height= 512;
-  glut_window->initGLUTWindow( "Visualization Library on GLUT - Rotating Cube", format, x, y, width, height );
+  qt4_window->initQt4Widget( "Visualization Library on Qt4 - Rotating Cube", format, NULL, x, y, width, height );
+  /* show the window */
+  qt4_window->show();
 
-  /* ... you can open more than one GLUT window! */
+  /* run the Win32 message loop */
+  int val = app.exec();
 
-  /* enter the GLUT main loop */
-  glutMainLoop();
+  /* deallocate the window with all the OpenGL resources before shutting down Visualization Library */
+  qt4_window = NULL;
 
-  /* this point is never reached since glutMainLoop() never returns! */
+  /* shutdown Visualization Library */
+  VisualizationLibrary::shutdown();
 
-  return 0;
+  return val;
 }
-// Have fun!
